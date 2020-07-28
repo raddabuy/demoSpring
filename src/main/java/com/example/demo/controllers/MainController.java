0a36b060c93f174842b51c3e88b41f5bb79a7bb5
raddabuy <raddabuy@yandex.ru>
2020-07-28 16:28:05 +0500

    templates

---
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "О нас");
+        return "about";
+    }
+
+}
