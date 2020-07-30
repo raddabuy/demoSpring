c6bc41a1881455163e9c44560fef79957dff8cfd
raddabuy <raddabuy@yandex.ru>
2020-07-30 15:28:47 +0500

    crud

--- src/main/java/com/example/demo/controllers/BlogController.java
+++ src/main/java/com/example/demo/controllers/BlogController.java
@@ -6,6 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -20,4 +26,58 @@ public class BlogController {
         model.addAttribute("posts", posts);
         return "blog-main";
     }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model){
+        return "blog-add";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post",res);
+        return "blog-details";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anouns, @RequestParam String full_text, Model model){
+        Post post = new Post(title, anouns, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(@PathVariable(value = "id") long id, Model model){
+        if(!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("post",res);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, @RequestParam String anouns, @RequestParam String full_text, Model model) throws Exception {
+        Post post = postRepository.findById(id).orElseThrow(() -> new Exception("Статья не найдена"));
+        post.setTitle(title);
+        post.setAnouns(anouns);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/delete")
+    public String blogPostDelete(@PathVariable(value = "id") long id, Model model) throws Exception {
+        Post post = postRepository.findById(id).orElseThrow(() -> new Exception("Статья не найдена"));
+        postRepository.delete(post);
+        return "redirect:/blog";
+    }
+
 }
--- src/main/java/com/example/demo/models/Post.java
+++ src/main/java/com/example/demo/models/Post.java
@@ -9,12 +9,22 @@ import javax.persistence.Id;
 @Entity
 public class Post {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String title, anouns, full_text;
     private int views;
+
+    public Post() {
+    }
+
+    public Post(String title, String anouns, String full_text) {
+        this.title = title;
+        this.anouns = anouns;
+        this.full_text = full_text;
+    }
 
     public Long getId() {
         return id;
